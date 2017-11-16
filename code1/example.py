e092f2c918dd2293c9f5d50dd55163088913bb77
cherry <taoying@wealink.com>
2017-11-16 15:47:04 +0800

    python

--- code1/example.py
+++ code1/example.py
@@ -1,8 +1,9 @@
-import sys
-
-
-print(dir())
-a=5
-print(dir())
-del a
-print(dir())
+#coding=utf-8
+import time
+from selenium import webdriver
+driver=webdriver.Firefox()
+driver.get("www.baidu.com")
+time.sleep(5)
+driver.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("su").click()
+driver.quit()
